ace8300b414b3746afc4becbc1e794af1377fa62
ways92 <d.ferdiansya@gmail.com>
2024-12-27 04:28:32 +0700

    penambahan tabel database method

--- src/middlewares/multerHandler.js
+++ src/middlewares/multerHandler.js
@@ -1,27 +1,23 @@
-import multer from 'multer'
-
+import multer from "multer";
 
 export const multerErrorHandler = (err, req, res, next) => {
-    if (err instanceof multer.MulterError) {
-      switch (err.code) {
-        case 'LIMIT_FILE_SIZE':
-          return res.status(400).json({ message: 'Ukuran file melebihi ukuran maksimum yang diizinkan (5MB)' });
-        case 'LIMIT_FILE_COUNT':
-          return res.status(400).json({ message: 'Terlalu banyak file. Hanya satu file yang diperbolehkan' });
-        case 'LIMIT_UNEXPECTED_FILE':
-          return res.status(400).json({ message: 'File yang tidak diharapkan. Silakan unggah file yang valid.' });
-        default:
-          return res.status(400).json({ message: `Multer error: ${err.message}` });
-      }
+  if (err instanceof multer.MulterError) {
+    switch (err.code) {
+      case "LIMIT_FILE_SIZE":
+        return res.status(400).json({ message: "Ukuran file melebihi ukuran maksimum yang diizinkan (5MB)" });
+      case "LIMIT_FILE_COUNT":
+        return res.status(400).json({ message: "Terlalu banyak file. Hanya satu file yang diperbolehkan" });
+      case "LIMIT_UNEXPECTED_FILE":
+        return res.status(400).json({ message: "File yang tidak diharapkan. Silakan unggah file yang valid." });
+      default:
+        return res.status(400).json({ message: `Multer error: ${err.message}` });
     }
-  
-    if (err) {
-      // Tangani error umum lainnya
-      console.error('Upload error:', err);
-      return res.status(500).json({ message: 'An internal server error occurred.', error: err.message });
-    }
-  
-    // Jika tidak ada error, lanjutkan ke middleware berikutnya
-    next();
-  };
-  
+  }
+
+  if (err) {
+    console.error("Upload error:", err);
+    return res.status(500).json({ message: "An internal server error occurred.", error: err.message });
+  }
+
+  next();
+};
